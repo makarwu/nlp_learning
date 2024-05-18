bb86fdaf19b42b64d3be775d7108c7965eb98176
Makar Wuckert <83913475+makarwu@users.noreply.github.com>
2024-05-18 16:06:16 +0200

    adjusted skipgram algwith example

--- word_vectors/skip-gram.py
+++ word_vectors/skip-gram.py
@@ -25,13 +25,102 @@ class word2vec(object):
             self.word_index[data[i]] = i
 
     def forward(self, X):
-        pass
+        self.h = np.dot(self.W.T, X).reshape(self.N, 1)
+        self.u = np.dot(self.W1.T, self.h)
+        print(self.u)
+        self.y = softmax(self.u)
+        return self.y
 
     def backward(self, x, t):
-        pass
+        e = self.y - np.asarray(t).reshape(self.V, 1) # e.shape is V x 1
+        dLdW1 = np.dot(self.h, e.T)
+        X = np.array(x).reshape(self.V, 1)
+        dLdW = np.dot(X, np.dot(self.W1, e).T)
+        self.W1 = self.W1 - self.alpha * dLdW1
+        self.W = self.W - self.alpha * dLdW
 
     def train(self, epochs):
-        pass
-
+        for x in range(1, epochs):
+            self.loss = 0
+            for j in range(len(self.X_train)):
+                self.forward(self.X_train[j])
+                self.backward(self.X_train[j], self.y_train[j])
+                C = 0
+                for m in range(self.V):
+                    if(self.y_train[j][m]):
+                        self.loss += -1 * self.u[m][0]
+                        C += 1
+                self.loss += C * np.log(np.sum(np.exp(self.u)))
+            print("epoch ", x, " loss = ", self.loss)
+            self.alpha *= 1 / ((1 + self.alpha * x))
+    
     def predict(self, word, number_of_predictions):
-        pass
+        if word in self.words:
+            index = self.word_index[word]
+            X = [0 for i in range(self.V)]
+            X[index] = 1
+            prediction = self.forward(X)
+            output = {}
+            for i in range(self.V):
+                output[prediction[i][0]] = i
+            
+            top_context_words = []
+            for k in sorted(output, reverse=True):
+                top_context_words.append(self.words[output[k]])
+                if(len(top_context_words) >= number_of_predictions):
+                    break
+            
+            return top_context_words
+        else:
+            print("Word not found in dictionary")
+    
+def preprocessing(corpus):
+    stop_words = set(stopwords.words('english'))
+    training_data = []
+    sentences = corpus.split('.')
+    for i in range(len(sentences)):
+        sentences[i] = sentences[i].strip()
+        sentence = sentences[i].split()
+        x = [word.strip(string.punctuation) for word in sentence if word not in stop_words]
+        x = [word.lower() for word in x]
+        training_data.append(x)
+    return training_data
+
+def prepare_data_for_training(sentences, w2v):
+    data = {}
+    for sentence in sentences:
+        for word in sentence:
+            if word not in data:
+                data[word] = 1
+            else:
+                data[word] += 1
+    V = len(data)
+    data = sorted(list(data.keys()))
+    vocab = {}
+    for i in range(len(data)):
+        vocab[data[i]] = i
+    for sentence in  sentences:
+        for i in range(len(sentences)):
+            center_word = [0 for x in range(V)]
+            center_word[vocab[sentence[i]]] = 1
+            context = [0 for x in range(V)]
+        for j in range(i - w2v.window_size, i + w2v.window_size):
+            if i != j and j >= 0 and j < len(sentence):
+                context[vocab[sentence[j]]] += 1
+        w2v.X_train.append(center_word)
+        w2v.y_train.append(context)
+    w2v.initialize(V, data)
+
+    return w2v.X_train, w2v.y_train
+
+corpus = ""
+corpus += "The earth revolves around the sun. The moon revolves around the earth"
+epochs = 1000
+
+training_data = preprocessing(corpus)
+w2v = word2vec()
+
+prepare_data_for_training(training_data, w2v)
+w2v.train(epochs)
+
+print(w2v.predict("around", 3))
