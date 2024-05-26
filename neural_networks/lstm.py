d1e321774a989c149ac94d4bf2fad03ec972b5a2
Makar Wuckert <83913475+makarwu@users.noreply.github.com>
2024-05-26 16:50:39 +0200

    added fw pass & train loop

--- neural_networks/lstm.py
+++ neural_networks/lstm.py
@@ -19,6 +19,7 @@ from re import A
 import numpy as np
 from collections import defaultdict
 from dataset import Dataset
+import matplotlib.pyplot as plt
 
 np.random.seed(42)
 
@@ -395,5 +396,234 @@ def clip_gradient_norm(grads, max_norm=0.25):
     
     return grads
 
+# Optimizer: Updating parameters using SGD
+
+def update_parameters(params, grads, lr=1e-3):
+    # Take a step
+    for param, grad in zip(params, grads):
+        param -= lr * grad
+    
+    return params
+
 def backward(z, f, i, g, C, o, h, v, outputs, targets, p = params):
-    pass
+    """
+    Arguments:
+    z -- your concatenated input data  as a list of size m.
+    f -- your forget gate computations as a list of size m.
+    i -- your input gate computations as a list of size m.
+    g -- your candidate computations as a list of size m.
+    C -- your Cell states as a list of size m+1.
+    o -- your output gate computations as a list of size m.
+    h -- your Hidden state computations as a list of size m+1.
+    v -- your logit computations as a list of size m.
+    outputs -- your outputs as a list of size m.
+    targets -- your targets as a list of size m.
+    p -- python list containing:
+                        W_f -- Weight matrix of the forget gate, numpy array of shape (n_a, n_a + n_x)
+                        b_f -- Bias of the forget gate, numpy array of shape (n_a, 1)
+                        W_i -- Weight matrix of the update gate, numpy array of shape (n_a, n_a + n_x)
+                        b_i -- Bias of the update gate, numpy array of shape (n_a, 1)
+                        W_g -- Weight matrix of the first "tanh", numpy array of shape (n_a, n_a + n_x)
+                        b_g --  Bias of the first "tanh", numpy array of shape (n_a, 1)
+                        W_o -- Weight matrix of the output gate, numpy array of shape (n_a, n_a + n_x)
+                        b_o --  Bias of the output gate, numpy array of shape (n_a, 1)
+                        W_v -- Weight matrix relating the hidden-state to the output, numpy array of shape (n_v, n_a)
+                        b_v -- Bias relating the hidden-state to the output, numpy array of shape (n_v, 1)
+    Returns:
+    loss -- crossentropy loss for all elements in output
+    grads -- lists of gradients of every element in p
+    """
+
+    # Unpack parameters
+    W_f, W_i, W_g, W_o, W_v, b_f, b_i, b_g, b_o, b_v = p
+
+    # Initialize gradients as zero
+    W_f_d = np.zeros_like(W_f)
+    b_f_d = np.zeros_like(b_f)
+
+    W_i_d = np.zeros_like(W_i)
+    b_i_d = np.zeros_like(b_i)
+
+    W_g_d = np.zeros_like(W_g)
+    b_g_d = np.zeros_like(b_g)
+
+    W_o_d = np.zeros_like(W_o)
+    b_o_d = np.zeros_like(b_o)
+
+    W_v_d = np.zeros_like(W_v)
+    b_v_d = np.zeros_like(b_v)
+    
+    # Set the next cell and hidden state equal to zero
+    dh_next = np.zeros_like(h[0])
+    dC_next = np.zeros_like(C[0])
+        
+    # Track loss
+    loss = 0
+    for t in reversed(range(len(outputs))):
+
+        # Compute the cross entropy
+        loss += -np.mean(np.log(outputs[t]) * targets[t])
+        # Get the previous hidden cell state
+        C_prev = C[t-1]
+
+        # Compute the derivative of the relation of the hidden-state to the output gate
+        dv = np.copy(outputs[t])
+        dv[np.argmax(targets[t])] -= 1
+
+        # Update the gradient of the relation of the hidden-state to the output gate
+        W_v_d += np.dot(dv, h[t].T)
+        b_v_d += dv
+
+        # Compute the derivative of the hidden state and output gate
+        dh = np.dot(W_v.T, dv)
+        dh += dh_next
+        do = dh * tanh(C[t])
+        do = sigmoid(o[t], derivative=True) * do
+
+        # Update the gradients with respect to the output gate
+        W_o_d += np.dot(do, z[t].T)
+        b_o_d += do
+
+        # Compute the derivative of the cell state and candidate g
+        dC = np.copy(dC_next)
+        dC += dh * o[t] * tanh(tanh(C[t]), derivative=True)
+        dg = dC * i[t]
+        dg = tanh(g[t], derivative=True) * dg
+        
+        # Update the gradients with respect to the candidate
+        W_g_d += np.dot(dg, z[t].T)
+        b_g_d += dg
+
+        # Compute the derivative of the input gate and update its gradients
+        di = dC * g[t]
+        di = sigmoid(i[t], True) * di
+        W_i_d += np.dot(di, z[t].T)
+        b_i_d += di
+
+        # Compute the derivative of the forget gate and update its gradients
+        df = dC * C_prev
+        df = sigmoid(f[t]) * df
+        W_f_d += np.dot(df, z[t].T)
+        b_f_d += df
+
+        # Compute the derivative of the input and update the gradients of the previous hidden and cell state
+        dz = (np.dot(W_f.T, df)
+             + np.dot(W_i.T, di)
+             + np.dot(W_g.T, dg)
+             + np.dot(W_o.T, do))
+        dh_prev = dz[:hidden_size, :]
+        dC_prev = f[t] * dC
+        
+    grads= W_f_d, W_i_d, W_g_d, W_o_d, W_v_d, b_f_d, b_i_d, b_g_d, b_o_d, b_v_d
+
+    # Clip gradients
+    grads = clip_gradient_norm(grads)
+
+    return loss, grads
+
+# Lets test the backward pass
+loss, grads = backward(z_s, f_s, i_s, g_s, C_s, o_s, h_s, v_s, outputs, targets_one_hot, params)
+print('We get a loss of: ', loss)
+
+## Lets build the Training Loop
+
+num_epochs = 200
+
+# Initialize a new network
+params = init_lstm(hidden_size=hidden_size, vocab_size=vocab_size, z_size=z_size)
+
+# Initialize the hidden state as 0
+hidden_state = np.zeros((hidden_size, 1))
+
+# Track loss
+training_loss, validation_loss = [], []
+
+for i in range(num_epochs):
+
+    # Track the loss
+    epoch_training_loss = 0
+    epoch_validation_loss = 0
+    
+    # For each sentence in validation set
+    for inputs, targets in validation_set:
+
+        # One-hot encode input and target sequence
+        inputs_one_hot = one_hot_encode_sequence(inputs, vocab_size)
+        targets_one_hot = one_hot_encode_sequence(targets, vocab_size)
+
+        # Initialize hidden state and cell state as zeros
+        h = np.zeros((hidden_size, 1))
+        c = np.zeros((hidden_size, 1))
+
+        # Forward pass
+        z_s, f_s, i_s, g_s, C_s, o_s, h_s, v_s, outputs = forward(inputs_one_hot, h, c, params)
+        
+        # Backward pass
+        loss, _ = backward(z_s, f_s, i_s, g_s, C_s, o_s, h_s, v_s, outputs, targets_one_hot, params)
+        
+        # Update loss
+        epoch_validation_loss += loss
+
+    # For each sentence in training set
+    for inputs, targets in training_set:
+        
+        # One-hot encode input and target sequence
+        inputs_one_hot = one_hot_encode_sequence(inputs, vocab_size)
+        targets_one_hot = one_hot_encode_sequence(targets, vocab_size)
+
+        # Initialize hidden state and cell state as zeros
+        h = np.zeros((hidden_size, 1))
+        c = np.zeros((hidden_size, 1))
+
+        # Forward pass
+        z_s, f_s, i_s, g_s, C_s, o_s, h_s, v_s, outputs = forward(inputs_one_hot, h, c, params)
+        
+        # Backward pass
+        loss, grads = backward(z_s, f_s, i_s, g_s, C_s, o_s, h_s, v_s, outputs, targets_one_hot, params)
+        
+        # Update parameters: Optimization
+        params = update_parameters(params, grads, lr=1e-1)
+        
+        # Update loss
+        epoch_training_loss += loss
+
+    # Save loss for plot
+    training_loss.append(epoch_training_loss/len(training_set))
+    validation_loss.append(epoch_validation_loss/len(validation_set))
+
+    # Print the loss for every 10 epochs
+    if i % 10 == 0:
+        print(f'Epoch {i}, training loss: {training_loss[-1]}, validation loss: {validation_loss[-1]}')
+
+# Get the first sentence in test set
+inputs, targets = test_set[1]
+
+# One-hot encode input and target sequence
+inputs_one_hot = one_hot_encode_sequence(inputs, vocab_size)
+targets_one_hot = one_hot_encode_sequence(targets, vocab_size)
+
+# Initialize the hidden state as zeros
+h = np.zeros((hidden_size, 1))
+c = np.zeros((hidden_size, 1))
+
+# Forward pass
+z_s, f_s, i_s, g_s, C_s, o_s, h_s, v_s, outputs = forward(inputs_one_hot, h, c, params)
+
+# Print example
+print('Input sentence:')
+print(inputs)
+
+print('\nTarget sequence:')
+print(targets)
+
+print('\nPredicted sequence:')
+print([idx_to_word[np.argmax(output)] for output in outputs])
+
+# Plot training and validation loss
+epoch = np.arange(len(training_loss))
+plt.figure()
+plt.plot(epoch, training_loss, 'r', label='Training loss',)
+plt.plot(epoch, validation_loss, 'b', label='Validation loss')
+plt.legend()
+plt.xlabel('Epoch'), plt.ylabel('NLL')
+plt.show()
