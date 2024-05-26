7eafd8c879e44888fa3b07f886d6b6421fd1d090
Makar Wuckert <83913475+makarwu@users.noreply.github.com>
2024-05-26 13:18:54 +0200

    added forward pass

--- neural_networks/lstm.py
+++ neural_networks/lstm.py
@@ -15,6 +15,7 @@ We process sequences in sequential manner. As such, the network will need
 to learn that e.g. t b's and an EOS token will follow 5 a's.
 """
 
+from re import A
 import numpy as np
 from collections import defaultdict
 from dataset import Dataset
@@ -124,5 +125,275 @@ training_set, validation_set, test_set = create_datasets(sequences, Dataset)
 print(f'We have {len(training_set)} samples in the training set.')
 print(f'We have {len(validation_set)} samples in the validation set.')
 print(f'We have {len(test_set)} samples in the test set.')
-  
 
+"""One-hot encodings:
+Simple function that return the one-hot encoded representation of a given index of a word in our 
+vocabulary. Notice that the shape of the one-hot encoding is equal to the entire vocabulary.
+Additionaly, we define a function to automatically one-hot encode a sentence.
+"""
+
+def one_hot_encode(index, vocab_size):
+    
+    # Initialize the encoded array
+    one_hot = np.zeros(vocab_size)
+    # Set the appropriate element to one
+    one_hot[index] = 1.0
+
+    return one_hot
+
+def one_hot_encode_sequence(sequence, vocab_size):
+
+    # Encode each word in sentence
+    encoding = np.array([one_hot_encode(word_to_idx[word], vocab_size) for word in sequence])
+    # Reshape encoding s.t it has shape (num words, vocab_size, 1)
+    encoding = encoding.reshape(encoding.shape[0], encoding.shape[1], 1)
+
+    return encoding
+
+test_word = one_hot_encode(word_to_idx['a'], vocab_size)
+print(f'Our one-hot encoding of \'a\' has shape {test_word.shape}.')
+
+test_sentence = one_hot_encode_sequence(['a', 'b'], vocab_size)
+print(f'Our one-hot encoding of \'a b\' has shape {test_sentence.shape}.')
+
+# Let's define the functions
+
+def sigmoid(x, derivative=False):
+ 
+    x_safe = x + 1e-12
+    f = 1 / (1 + np.exp(-x_safe))
+    
+    if derivative: # Return the derivative of the function evaluated at x
+        return f * (1 - f)
+    else: # Return the forward pass of the function at x
+        return f
+
+def tanh(x, derivative=False):
+ 
+    x_safe = x + 1e-12
+    f = (np.exp(x_safe)-np.exp(-x_safe))/(np.exp(x_safe)+np.exp(-x_safe))
+    
+    if derivative: # Return the derivative of the function evaluated at x
+        return 1-f**2
+    else: # Return the forward pass of the function at x
+        return f
+
+def softmax(x, derivative=False):
+ 
+    x_safe = x + 1e-12
+    f = np.exp(x_safe) / np.sum(np.exp(x_safe))
+    
+    if derivative: # Return the derivative of the function evaluated at x
+        pass # We will not need this one
+    else: # Return the forward pass of the function at x
+        return f
+
+
+### Now we can move on with the LSTMs
+
+hidden_size = 50 # the number of dimensions in the hidden state
+vocab_size = len(word_to_idx) # Size of the vocabulary used
+
+def init_orthogonal(param):
+    """
+    Intializes weight paramters orthogonally.
+    This is a common initialization for recurrent neural networks.
+    Refers to this paper: https://arxiv.org/abs/1312.6120
+    """
+    if param.ndim < 2:
+        raise ValueError("Only parameters with 2 or more dimensions are supported.")
+    
+    rows, cols = param.shape
+    new_param = np.random.randn(rows, cols)
+    if rows < cols:
+        new_param = new_param.T
+    
+    # Compute QR factorization
+    q, r = np.linalg.qr(new_param)
+
+    # Make Q uniform
+    d = np.diag(r, 0)
+    ph = np.sign(d)
+    q *= ph
+
+    if rows < cols:
+        q = q.T
+    
+    new_param = q
+
+    return new_param
+
+# Initialization of the LSTM network
+
+# Size of concatenated hidden + input vector
+z_size = hidden_size + vocab_size # the dimensions of the concatenated input
+
+def init_lstm(hidden_size, vocab_size, z_size):
+
+    # Weight matrix (forget gate)
+    W_f = np.zeros((hidden_size, z_size))
+
+    # Bias for forget gate
+    b_f = np.zeros((hidden_size, 1))
+
+    # Weight matrix (input gate)
+    W_i = np.zeros((hidden_size, z_size))
+
+    # Bias for the input gate
+    b_i = np.zeros((hidden_size, 1))
+
+    # Weight matrix (candidate)
+    W_g = np.zeros((hidden_size, z_size))
+
+    # Bias for the candidate
+    b_g = np.zeros((hidden_size, 1))
+
+    # Weight matrix for the output gate
+    W_o = np.zeros((hidden_size, z_size))
+
+    # Bias for the output gate
+    b_o = np.zeros((hidden_size, 1))
+
+    # Weight matrix for the hidden-state to the output
+    W_v = np.zeros((vocab_size, hidden_size))
+
+    # Bias for the logits
+    b_v = np.zeros((vocab_size, 1))
+
+    # Initialize the weights
+    W_f = init_orthogonal(W_f)
+    W_i = init_orthogonal(W_i)
+    W_g = init_orthogonal(W_g)
+    W_o = init_orthogonal(W_o)
+    W_v = init_orthogonal(W_v)
+
+    return W_f, W_i, W_g, W_o, W_v, b_f, b_i, b_g, b_o, b_v
+
+params = init_lstm(hidden_size=hidden_size, vocab_size=vocab_size, z_size=z_size)
+print('W_f:', params[0].shape)
+print('W_i:', params[1].shape)
+print('W_g:', params[2].shape)
+print('W_o:', params[3].shape)
+print('W_v:', params[4].shape)
+print('b_i:', params[5].shape)
+print('b_g:', params[6].shape)
+print('b_o:', params[7].shape)
+print('b_v:', params[8].shape)
+
+for param in params:
+    assert param.ndim == 2, \
+        'all parameters should be 2-dimensional' \
+        '(hint: a dimension can simply have size 1)'
+
+## Forward Pass:
+def forward(inputs, h_prev, C_prev, p):
+    
+    assert h_prev.shape == (hidden_size, 1)
+    assert C_prev.shape == (hidden_size, 1)
+
+    # First we unpack our parameters
+    W_f, W_i, W_g, W_o, W_v, b_f, b_i, b_g, b_o, b_v = p
+
+    # Save a list of computations for each of the components in the LSTM
+    x_s, z_s, f_s, i_s,  = [], [] ,[], []
+    g_s, C_s, o_s, h_s = [], [] ,[], []
+    v_s, output_s =  [], [] 
+
+    # Append the initial cell and hidden state to their respective lists
+    h_s.append(h_prev)
+    C_s.append(C_prev)
+
+    for x in inputs:
+
+        # Concatenate input and hidden state
+        z = np.row_stack((h_prev, x))
+        z_s.append(z)
+
+        # Calculate forget gate
+        f = sigmoid(np.dot(W_f, z) + b_f)
+        f_s.append(f)
+
+        # Calculate the input gate
+        i = sigmoid(np.dot(W_i, z) + b_i)
+        i_s.append(i)
+
+        # Calculate memory candidate
+        g = tanh(np.dot(W_g, z) + b_g)
+        g_s.append(g)
+
+        # Calculate memory state
+        C_prev = f * C_prev + i * g
+        C_s.append(C_prev)
+
+        # Calculate the output gate
+        o = sigmoid(np.dot(W_o, z) + b_o)
+        o_s.append(o)
+
+        # Calculate the hidden state
+        h_prev = o * tanh(C_prev)
+        h_s.append(h_prev)
+
+        # Calculate logits
+        v = np.dot(W_v, h_prev) + b_v
+        v_s.append(v)
+
+        # Calculate softmax
+        output = softmax(v)
+        output_s.append(output)
+
+    return z_s, f_s, i_s, g_s, C_s, o_s, h_s, v_s, output_s
+
+print('Lets test the forward pass...')
+
+# Get first sentence in test set
+inputs, targets = test_set[1]
+
+# One-hot encode input and target sequence
+inputs_one_hot = one_hot_encode_sequence(inputs, vocab_size)
+targets_one_hot = one_hot_encode_sequence(targets, vocab_size)
+
+# Initialize hidden state as zeros
+h = np.zeros((hidden_size, 1))
+c = np.zeros((hidden_size, 1))
+
+# Forward pass
+z_s, f_s, i_s, g_s, C_s, o_s, h_s, v_s, outputs = forward(inputs_one_hot, h, c, params)
+
+output_sentence = [idx_to_word[np.argmax(output)] for output in outputs]
+print('Input sentence:')
+print(inputs)
+
+print('\nTarget sequence:')
+print(targets)
+
+print('\nPredicted sequence:')
+print([idx_to_word[np.argmax(output)] for output in outputs])
+
+## Now we implement the backward pass:
+## we additionally clip the gradients to prevent gradients exploding problem
+
+def clip_gradient_norm(grads, max_norm=0.25):
+   
+    # Set the maximum of the norm to be of type float
+    max_norm = float(max_norm)
+    total_norm = 0
+    
+    # Calculate the L2 norm squared for each gradient and add them to the total norm
+    for grad in grads:
+        grad_norm = np.sum(np.power(grad, 2))
+        total_norm += grad_norm
+    
+    total_norm = np.sqrt(total_norm)
+    
+    # Calculate clipping coeficient
+    clip_coef = max_norm / (total_norm + 1e-6)
+    
+    # If the total norm is larger than the maximum allowable norm, then clip the gradient
+    if clip_coef < 1:
+        for grad in grads:
+            grad *= clip_coef
+    
+    return grads
+
+def backward(z, f, i, g, C, o, h, v, outputs, targets, p = params):
+    pass
